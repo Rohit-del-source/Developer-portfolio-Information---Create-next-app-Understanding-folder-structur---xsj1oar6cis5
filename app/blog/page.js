6d219e7b8ecb3bae29e0a3865e8faf21eb86365c
Rohit-del-source <rohitverma18158920@gmail.com>
2024-01-09 12:28:26 +0530

    third

--- app/blog/page.js
+++ app/blog/page.js
@@ -10,9 +10,8 @@ const Blog = () => {
             <h2>Checkout my awesome blogs </h2>
             <h3><a href='https://www.newtonschool.co/post/top-13-web-developer-interview-questions'>Web Development Interview questions</a></h3>
             <h3><a href='https://www.newtonschool.co/post/graph-data-structure-explained-with-examples'>Graph Data Structures</a></h3>
-            <Link href="/"><button id="home-to-blog"> Go to Home</button></Link> 
-           <Link href="/info"><button id="home-to-info"> Go to Info</button></Link> 
-        
+            <Link href="/info"><button id="blog-to-info"> Go to Info</button></Link>
+            <Link href="/"><button id="blog-to-home"> Go to Home</button></Link>
         </div>
     )
 }
